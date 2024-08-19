a930172902648374b399f0c99a2f3924a6ea540b
Augustus2011 <kun.kerdthaisong@gmail.com>
2024-08-19 10:28:48 +0700

    aa

---
+++ thai_gov_ocr.v1i.yolov8/README.dataset.txt
@@ -0,0 +1,6 @@
+# thai_gov_ocr > 2024-08-01 3:41am
+https://universe.roboflow.com/hack2liver/thai_gov_ocr
+
+Provided by a Roboflow user
+License: BY-NC-SA 4.0
+
---
+++ yolo_predict.py
@@ -0,0 +1,16 @@
+#model
+from ultralytics import YOLO
+
+#mange images,tensor,numpy
+import torch
+import pandas as pd
+import numpy as np
+import cv2
+
+#manage file,path
+import glob
+import os
+
+#etc
+import tqdm
+
